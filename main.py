8c7e3c61a34d2ec66c9d67033d80098326b5b1e4
gowtham <gowtham@sportai.co.in>
2023-10-05 11:51:03 +0530

    'first'

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load Titanic dataset
 data = pd.read_csv('train.csv')
 
 # Preprocess the dataset (feature engineering, etc.)
+# For simplicity, we'll use Pclass as a feature and Survived as the label
+features = data[['Pclass']]
+labels = data['Survived']
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Train the model with different learning rates
-learning_rates = 0.01
+# Create a bar plot showing the distribution of passengers by class
+class_distribution = data['Pclass'].value_counts()
+plt.figure(figsize=(8, 6))
+sns.barplot(x=class_distribution.index, y=class_distribution.values)
+plt.xlabel('Class')
+plt.ylabel('Count')
+plt.title('Passenger Class Distribution')
+plt.savefig('plots/class_distribution.png')
+
+# Train the model with different learning rates (dummy example)
+learning_rates = [0.01, 0.1, 0.2]
 
 for lr in learning_rates:
     model = RandomForestClassifier(learning_rate=lr)
@@ -25,19 +38,8 @@ for lr in learning_rates:
 # Ensure the 'plots' directory exists before running the script
 # ...
 
-import seaborn as sns
-
-# Load Titanic dataset
-data = pd.read_csv('titanic.csv')
-
-# Create a bar plot showing the distribution of passengers by class
-class_distribution = data['Pclass'].value_counts()
-plt.figure(figsize=(8, 6))
-sns.barplot(x=class_distribution.index, y=class_distribution.values)
-plt.xlabel('Class')
-plt.ylabel('Count')
-plt.title('Passenger Class Distribution')
-plt.savefig('plots/class_distribution.png')
-
-# Other code for model training and evaluation...
-
+plt.plot([1, 2, 3], [1, 2, 3])
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Example Plot')
+plt.savefig('plots/example_plot.png')
