11db07c5c9945f6a49b17ff1c2e2fef7bfc7054e
gowtham <gowtham@sportai.co.in>
2023-10-05 13:00:08 +0530

    '9th'

--- main.py
+++ main.py
@@ -4,12 +4,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 import os
 # Load the dataset
 df = pd.read_csv('50_Startups.csv')
 os.makedirs('plots', exist_ok=True)
+state_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
 
+# Define a column transformer to apply transformations to specific columns
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('state', state_transformer, ['State'])
+    ],
+    remainder='passthrough'
+)
+
+# Create a pipeline for preprocessing and modeling
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('regressor', LinearRegression())
+])
 # Function to fit and plot regression with given test size
 def fit_and_plot(test_size):
     # Split data into training and testing sets
