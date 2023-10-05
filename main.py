83686d374fe9d9545b284fe0bd05c17bfd2ab020
gowtham <gowtham@sportai.co.in>
2023-10-05 11:57:11 +0530

    'fourth'

--- main.py
+++ main.py
@@ -9,39 +9,25 @@ import os
 data = pd.read_csv('train.csv')
 os.makedirs('plots', exist_ok=True)
 
-# Preprocess the dataset (feature engineering, etc.)
-# For simplicity, we'll use Pclass as a feature and Survived as the label
-features = data[['Pclass']]
-labels = data['Survived']
+
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Create a bar plot showing the distribution of passengers by class
-class_distribution = data['Pclass'].value_counts()
-plt.figure(figsize=(8, 6))
-sns.barplot(x=class_distribution.index, y=class_distribution.values)
-plt.xlabel('Class')
-plt.ylabel('Count')
-plt.title('Passenger Class Distribution')
-plt.savefig('plots/class_distribution.png')
 
-# Train the model with different learning rates (dummy example)
-learning_rates = [0.01, 0.1, 0.2]
 
-for lr in learning_rates:
-    model = RandomForestClassifier(learning_rate=lr)
+num_estimators = [50, 100, 150]
+
+for n_estimators in num_estimators:
+    model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
     model.fit(X_train, y_train)
     # Evaluate the model and save performance metrics or plots (if needed)
 
-# Save plots
-# For simplicity, let's assume you generate plots and save them in the 'plots' directory
-# Adjust this based on how you generate plots in your specific use case
-# Ensure the 'plots' directory exists before running the script
-# ...
-
-plt.plot([1, 2, 3], [1, 2, 3])
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Example Plot')
-plt.savefig('plots/example_plot.png')
+    # Save the plot to the 'plots' directory
+    plt.figure(figsize=(8, 6))
+    sns.barplot(x=class_distribution.index, y=class_distribution.values)
+    plt.xlabel('Class')
+    plt.ylabel('Count')
+    plt.title(f'Passenger Class Distribution (n_estimators={n_estimators})')
+    plt.savefig(f'plots/class_distribution_{n_estimators}.png')
+    plt.close()
